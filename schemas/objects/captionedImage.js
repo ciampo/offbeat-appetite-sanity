4b5f18a639f3efb5de4d0d7509df84a692bcf5f7
Marco Ciampini <marco.ciampo@gmail.com>
2020-03-01 18:34:37 +0100

    custom captioned image preview

--- schemas/objects/captionedImage.js
+++ schemas/objects/captionedImage.js
@@ -1,9 +1,27 @@
+import React from 'react';
+
 import { generateImageField } from '../common/image';
 
-export default generateImageField({
-  name: 'captionedImage',
-  title: 'Captioned image',
-  description: 'An image with an optional caption',
-  altText: true,
-  caption: true,
-});
+export default {
+  ...generateImageField({
+    name: 'captionedImage',
+    title: 'Captioned image',
+    description: 'An image with an optional caption',
+    altText: true,
+    caption: true,
+  }),
+  preview: {
+    select: {
+      alt: 'alt',
+      caption: 'caption',
+      url: 'asset.url',
+    },
+    prepare(selection) {
+      return {
+        title: `[image] ${selection.alt}`,
+        subtitle: selection.caption,
+        media: <img src={selection.url} alt={selection.alt} />,
+      };
+    },
+  },
+};
