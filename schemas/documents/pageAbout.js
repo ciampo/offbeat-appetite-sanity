d1b3a400d90536b087a7079ff811ac97bdeeafe7
Marco Ciampini <marco.ciampo@gmail.com>
2020-05-25 15:01:13 +0200

    about page: content converted to rich portable text, added hero specific title

--- schemas/documents/pageAbout.js
+++ schemas/documents/pageAbout.js
@@ -19,14 +19,22 @@ export default {
       name: 'title',
       type: 'string',
       title: 'Title',
-      description: 'The title shown in the page',
+      description: 'The title of the page (used in nav)',
+      validation: (Rule) => Rule.required(),
+    },
+
+    {
+      name: 'heroTitle',
+      type: 'string',
+      title: 'Hero Title',
+      description: 'The title shown in the page hero',
       validation: (Rule) => Rule.required(),
     },
 
     // Body content
     {
       name: 'content',
-      type: 'simplePortableText',
+      type: 'richPortableText',
       title: 'Content',
       description: 'The main content of the page',
       validation: (Rule) => Rule.required(),
